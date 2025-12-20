f2469b22b54d2ee2476bc74f014de2d2d5452fcc
hatsune_kinoshita <hatsune.k.pc8010@gmail.com>
2025-12-20 03:04:09 -0800

    レストラン結果表示コンポーネント作成

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import AdminPage from "./pages/admin.tsx";
 import LP from "./pages/LandingPage.tsx";
 import Participant from "./pages/Participant.tsx";
+import Restaurant from "./components/Restaurant.tsx";
 
 export default function App() {
   return (
@@ -10,6 +11,8 @@ export default function App() {
       <Route index element={<LP />} />
       <Route path="/admin" element={<AdminPage />} />
       <Route path= "/participant" element = {<Participant />} />
+      {/* 結果表示できるかのテスト */}
+      <Route path= "/restaurant" element = {<Restaurant />} /> 
     </Routes>
   );
 }
